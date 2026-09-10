fef6312766f8151276ef843060b824289600502c
Venicius Farias <eng.venicius@gmail.com>
2026-09-09 22:00:06 -0300

    implementar interface minimalista

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('node:http');
+
+JSON.stringify();
+
+const user = {
+    name: 'Venicius Farias',
+    email: 'venicius@gmail.com'
+};
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'application/json'});
+    res.end(JSON.stringify({
+        name: 'Venicius Farias',
+        email: 'eng.venicius@gmail.com',
+        age: 29
+    }));
+}).listen(3000);
